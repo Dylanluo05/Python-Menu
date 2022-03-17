2e99309a67985631962f1d824e09e5482d1292e5
Dylan Luo <dragonfly.luo@gmail.com>
2022-03-17 19:21:09 +0000

    Created Python Menu

--- main.py
+++ main.py
@@ -1,66 +1,73 @@
-"""
-Introduction to Console Programming
-Writing a function to print a menu
-"""
+import swap
+import christmastree
+import keypad
+import badboat
+import goodboat
 
 
-# Menu options in print statement
-def print_menu1():
-    print('1 -- Add' )
-    print('2 -- Subtract' )
-    print('3 -- Multiply' )
-    print('4 -- Exit' )
-    runOptions()
+main_menu = [
+    ["Swap", swap.run],
+    ["Keypad", keypad.run],
+]
+
+drawingsub_menu = [
+    ["Christmas Tree", christmastree.options]
+]
+
+animationsub_menu = [
+    ["1st Boat", badboat.run],
+    ["2nd Boat", goodboat.ship]
+]
+
+border = "=" * 25
+banner = f"\n{border}\nPlease Select An Option\n{border}"
 
 
-# Menu options as a dictionary
-menu_options = {
-    1: 'Stringy',
-    2: 'Numby',
-    3: 'Listy',
-    4: 'Exit',
-}
-
-# Print menu options from dictionary key/value pair
-def print_menu2():
-    for key in menu_options.keys():
-        print(key, '--', menu_options[key] )
-    runOptions()
-
-# menu option 1
-def stringy():
-    print('You chose \' 1 -  Stringy\'')
-
-# menu option 2
-def numby():
-    print('You chose \' 2 - Numby\'')
-
-# menu option 3
-def listy():
-    print('You chose \'3 - Listy\'')
-
-
-# call functions based on input choice
-def runOptions():
-    # infinite loop to accept/process user menu choice
-    while True:
+def buildMenu(banner, options):
+    print(banner)
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
+    for key, value in prompts.items():
+        print(key, '->', value[0])
+    choice = input("Type your choice> ")
+    try:
+        choice = int(choice)
+        if choice == 0:
+            return
         try:
-            option = int(input('Enter your choice 1-4: '))
-            if option == 1:
-                stringy()
-            elif option == 2:
-                numby()
-            elif option == 3:
-                listy()
-            # Exit menu    
-            elif option == 4:  
-                print('Exiting! Thank you! Good Bye...')
-                exit() # exit out of the (infinite) while loop
-            else:
-                print('Invalid option. Please enter a number between 1 and 4.')
-        except ValueError:
-            print('Invalid input. Please enter an integer input.')
+            action = prompts.get(choice)[1]
+            action()
+        except TypeError:
+            try:
+                exec(open(action).read())
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+    except ValueError:
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        print(f"Invalid Choice: {choice}")
+    buildMenu(banner, options)
 
-if __name__=='__main__':
-    # print_menu1()
-    print_menu2()
+
+def drawingsubmenu():
+    title = "Function Submenu" + banner
+    buildMenu(title, drawingsub_menu)
+
+
+def animationsubmenu():
+    title = "Function Submenu" + banner
+    buildMenu(title, animationsub_menu)
+
+
+def menu():
+    title = "Function Menu" + banner
+    menu_list = main_menu.copy()
+    menu_list.append(["Tree", drawingsubmenu])
+    menu_list.append(["Python Animations", animationsubmenu])
+    buildMenu(title, menu_list)
+
+
+if __name__ == "__main__":
+    menu()
